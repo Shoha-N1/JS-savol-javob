74f5048cc93a0813b23b3c2a64d5fe0e5188c8f2
Shoha-N1 <abduvohidovs85@gmail.com>
2022-10-11 15:32:57 +0500

    Commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+ var elHeading = document.querySelector("#first");
+
+ var number = Number ( prompt( "Jami pulingiz ?") );
+
+ if ( number >= 9565) {
+   elHeading.textContent = "Oq yo'l, Jonibek!"
+ }
+
+ else {
+   elHeading.textContent = "Jonibek, Shahzoddan so'ra, balki berar! U ham bermasa bollardan so'ra! Ularam bermasa uyda telefonda kino ko'r."
+ };
+
+ 
